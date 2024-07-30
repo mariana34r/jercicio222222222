239a5a1fdc79e622e64feaf54d29e1f26b99c80a
Kevinromero04 <kevinromerorincon7@gmail.com>
2024-07-30 06:40:44 -0500

    feat: :sparkles: Creacion de la funcion registro
    esta funcion sirve para registyrar las ciudades y sus datos

--- registro.py
+++ registro.py
@@ -1,8 +1,15 @@
 import cragar_guardar as gg
 
 def regis_citi():
-    data  =["1. parads", "2.k mmfvmfd", "3.vmlsdmv"]
-    for n in data:
-        print(n)
-
+    data  = gg.cargar_ciudades()
+    datos = {}
+    cd_postal = int(input("Digite el codigo postal de la ciudad: "))
+    if str(cd_postal) in data["ciudades"]:
+        print("Ya hay una ciudad con este codigo postal")
+    else:
+        datos["Nombre"] = str(input("Digite el nombre de la ciudad: "))
+        datos["Pais"] = str(input("Digite el Pais de la ciudad: "))
+        datos["Poblacion"] = int(input("Digite la poblacion de la ciudad: "))
+        data["ciudades"][cd_postal] = datos
+        gg.guarda_ciudades(data)
 regis_citi()
